8562dd01b2c4058bb5f6d42e393bbef5e0103533
ionutbaur <ionut.baur@gmail.com>
2026-04-09 13:47:02 +0300

    added tests to check the auth type

--- src/test/java/ro/ionutzbaur/thermostat/rest/ThermostatResourceTest.java
+++ src/test/java/ro/ionutzbaur/thermostat/rest/ThermostatResourceTest.java
@@ -1,12 +1,25 @@
 package ro.ionutzbaur.thermostat.rest;
 
+import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import io.smallrye.mutiny.Uni;
+import jakarta.ws.rs.core.Form;
+import org.apache.http.HttpStatus;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
+import ro.ionutzbaur.thermostat.datasource.tado.entity.auth.DeviceAuthorizationResponse;
+import ro.ionutzbaur.thermostat.datasource.tado.service.TadoAuthService;
 import ro.ionutzbaur.thermostat.interceptor.filter.BrandFilter;
+import ro.ionutzbaur.thermostat.model.auth.AuthInitiateResponse;
+import ro.ionutzbaur.thermostat.model.auth.AuthInitiateType;
 import ro.ionutzbaur.thermostat.model.enums.Brand;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 /**
  * This will test that for a given brand provided on header,
@@ -15,7 +28,40 @@ import static io.restassured.RestAssured.given;
 @QuarkusTest
 class ThermostatResourceTest {
 
+    @InjectMock
+    @RestClient
+    TadoAuthService tadoAuthService;
+
+    private static final String AUTH_PATH = "base/auth";
     private static final String USER_INFO_PATH = "base/user-info";
+
+    @Test
+    void initiateAuthentication_TadoShouldReturn_DEVICE_AUTH() {
+        when(tadoAuthService.initiateDeviceAuth(any(Form.class)))
+                .thenReturn(Uni.createFrom().item(new DeviceAuthorizationResponse()));
+
+        AuthInitiateResponse authInitiateResponse = initiateAuthentication(Brand.TADO);
+        assertEquals(AuthInitiateType.DEVICE_AUTH, authInitiateResponse.getType());
+    }
+
+    @Test
+    void initiateAuthentication_ZigbeeShouldReturn_CREDENTIALS_REQUIRED() {
+        AuthInitiateResponse authInitiateResponse = initiateAuthentication(Brand.ZIGBEE);
+        assertEquals(AuthInitiateType.CREDENTIALS_REQUIRED, authInitiateResponse.getType());
+    }
+
+    private AuthInitiateResponse initiateAuthentication(Brand brand) {
+        return given()
+                .when()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header(BrandFilter.BRAND_ID_HEADER, brand)
+                .post(AUTH_PATH)
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .as(AuthInitiateResponse.class);
+    }
 
     @Test
     void getUserInfo_TadoHeader() {
